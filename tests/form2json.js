7a0705c65f57889734b5470a40e21e0b46f32ab0
Glutexo <driezasson@me.com>
2014-06-01 15:10:34 +0200

    Test empty inputs.

--- tests/form2json.js
+++ tests/form2json.js
@@ -14,3 +14,18 @@ test('plain vars are captured', function() {
         unknown: 'watermelon'
     });
 });
+
+test('empty vars are captured', function() {
+    var html = '<form>\n';
+    html += '   <input type="hidden" name="nothing" value="" />\n'
+    html += '   <input type="hidden" name="nihilism" />\n'
+    html += '</form>\n';
+
+    var $form = $(html);
+    var formData = getFormData($form);
+
+    deepEqual(formData, {
+        nothing: '',
+        nihilism: ''
+    });
+});
